999e370a8dbf4b35896981c76ed2eb815d3d1736
Xingrui Pan <2190620593@qq.com>
2026-09-11 09:16:17 +0800

    Update build.ps1
    rename

--- module5/build.ps1
+++ module5/build.ps1
@@ -1,5 +1,5 @@
 $ErrorActionPreference = 'Stop'
-Push-Location (Join-Path $PSScriptRoot '../main(测试占位逻辑)')
+Push-Location (Join-Path $PSScriptRoot '../main')
 try {
     & g++ -static -std=c++20 -Wall -Wextra -finput-charset=UTF-8 -fexec-charset=UTF-8 -I ../module5 main.cpp game.cpp room.cpp ../module5/CommandParser.cpp ../Player.cpp ../Item.cpp ../SaveManager.cpp -o MudOUC.exe
     if ($LASTEXITCODE -ne 0) { throw 'Game build failed' }
